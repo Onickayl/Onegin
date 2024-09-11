540a4c6af3f7ff022ff26487621227c6bcfdb792
Onickayl <elizabetafox@yandex.ru>
2024-09-11 18:39:29 +0300

    change place of str

--- vers1.cpp
+++ vers1.cpp
@@ -4,18 +4,27 @@
 
 int main()
 {
-    const char* first_str = "My uncle keeps to honest systems: By falling ill yet not in jest,";
+    const char* first_str = "He made keeps to honest systems: By falling ill yet not in jest,";
     const char* second_str = "He made me love him with insistence And couldn’t find some better test.";
+    const char* elem = "";
 
-    printf("%c - %d\n", first_str[0], first_str[0]);
+    printf("%s\n", first_str);
 
-    printf("%c - %d\n", second_str[0], second_str[0]);
+    printf("%s\n", second_str);
 
-    int m = first_str[0] - second_str[0];
 
-    int cmp = strcmp(first_str, second_str);
+    if (int cmp = strcmp(first_str, second_str) < 0)
+    {
+        elem = second_str;
+        second_str = first_str;
+        first_str = elem;
+    }
 
-    printf("%d %d\n", m, cmp);
+    printf("\n");
+
+    printf("%s\n", first_str);
+
+    printf("%s\n", second_str);
 
     return 0;
 }
