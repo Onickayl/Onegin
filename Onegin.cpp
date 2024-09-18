dee9b89f1a93d4ba60ed16960a7f0406e5068e35
Onickayl <elizabetafox@yandex.ru>
2024-09-18 21:59:05 +0300

    something

--- Onegin.cpp
+++ Onegin.cpp
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <TXLib.h>
+#include <ctype.h>
 
 
 
-// А может все маленькие:   int tolower(int с);
+// разбиение на функции и файлы. ассерты
+
+
+int My_strcmp(char* array[7]);
+
 
 // Возвращает true, если с - буква:   int isalpha(int с);
 
 
 int main()
 {
+   // int if_letter = 0;
 
     FILE *file = fopen("Onegin.txt", "r");
 
@@ -25,6 +31,7 @@ int main()
 
     char* array[7] = {};
 
+
     printf("Исходник\n");
 
     // read file and full of array
@@ -32,55 +39,23 @@ int main()
     for(int i = 0; i < 7; i ++)
     {
         char* buffer = (char*) calloc(500, sizeof(char));
-        fgets(buffer, 500, file);     // len of string <= 500
-
-        //printf("%p ", buffer);   // адрес, что лежит в ячейке buff
-        //printf("%p ", &buffer); // адрес ячейки buff
-
-        //printf("%d ", *buffer);   // код первой буквы строки
-        //printf("%c\n", *buffer);        // печатает букву
+        fgets(buffer, 500, file);
 
         printf("%s", buffer);
 
-        // превращение в маленькие
-        for(int k = 0; k < 500; k++)
-        {
-            buffer[k] = tolower(buffer[k]);
-            //printf("%c ", buffer[k]);
-        }
-
-
-        //printf("%s", buffer);     // печатает строку
-
         array[i] = buffer;
 
-        //printf("%c\n", *array[i]);    // буква
-        //printf("%p\n", array[i]);
     }
-
 
     printf("\n\n");
 
-    // исходный текст
-
-    printf("Маленькие\n");
-    for(int j = 0; j < 7; j ++)
-        printf("%s", array[j]);
-
-    // текст в больших буквах
-
-    //int toupper(int *buffer);
-
-    // сортировка
-
     char* free_str = "";
-
 
     for(int j = 0; j < 7; j ++)
     {
         for(int i = 0; i < 6; i ++)
         {
-            if(strcmp(array[i], array[i+1]) > 0)
+            if(My_strcmp(array) > 0)        // my_strcmp
             {
                 free_str = array[i+1];
                 array[i+1] = array[i];
@@ -89,10 +64,10 @@ int main()
         }
     }
 
-    printf("\n\n");
 
 
     // отсортированный текст
+
     printf("Отсортированный\n");
     for(int j = 0; j < 7; j ++)
         printf("%s", array[j]);
@@ -106,9 +81,35 @@ int main()
 
 
 
+int My_strcmp(char* array[7])
+{
 
-    // getchar()
+    for(int i = 0; i < 7; i++)
+    {
+        for(int j = 0; j < 100; j++)
+        {
+            array[i][j] = tolower(array[i][j]);     // small letters
 
 
-    // сортировка по алфавиту
-    // strcmp() - сравнение, возвращает разницу чисел > 0 первое больше, < 0 второе больше
+            int if_letter = isalpha(array[i][j]);
+
+            if (if_letter == 0)
+            {
+                for(int k = 0; k < 100; ++k)
+                {
+                    array[i][k - 1] = array[i][k];
+                }
+            }
+        }
+    }
+
+
+
+    for(int i = 0; i < 7; i++)
+    {
+        int compare = strcmp(array[i], array[i+1]);
+        return compare;
+    }
+
+}
+
